f6b034c3f3014d767d1126ca6615e6b4b97d48a3
nimohLee <spakers38@gmail.com>
2022-07-25 09:25:41 +0900

    cm

---
+++ 3rd_Project/src/com/naver/Hello.java
@@ -0,0 +1,14 @@
+package com.naver;
+
+import java.util.Scanner; // ex) 'java'라는 project 안에 'util' package 안에 'Scanner' class
+
+public class Hello {
+	Scanner sc;
+	
+	// 패키지는
+	// 기능별로 구분하는 용도 (폴더 개념)
+	// hs.ui
+	// Frame
+	// hs.ui.detail ( 'hs'라는 project안에 'ui' package 안에 'detail'이라는 클래스를 말함 )
+	// color
+}
---
+++ 4th_Project/src/GUIEx1.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class GUIEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame(); // JFrame (창 만들기) 생성
+	
+		f.setVisible(true); // JFrame의 인스턴스 f를 활용. 화면에 보이게 하기
+	
+	}
+
+}
